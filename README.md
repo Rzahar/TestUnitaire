bd6549c7a5f2c4a375aeb4ee9154f383afc7fb90
Jeremie <thery.jeremie@yahoofr>
2021-12-21 09:48:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Projet d'exemple pour les tests avec Spring Boot
+
+Ce projet montre un exemple de projet test avec une API faite avec Spring Boot
---
+++ src/test/java/fr/jeremie/testdemo/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jeremie.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
